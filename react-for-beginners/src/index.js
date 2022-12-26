ed6590db521bf42a74f6386f05872aacc9757098
KJS <nus0503@naver.com>
2022-12-27 05:20:30 +0900

    2022/12/27 about useEffect

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //  <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  //  </React.StrictMode> //    <- 리액트 앱을 최소 생성하면 기본적으로 생성되는 태그! 리액트에서 제공하는 검사 도구로 개발 모드일때만 디버그를 하며 해당 태그로 감싸져 있는 부분은 자손까지 검사한다
+);                    //    
 
