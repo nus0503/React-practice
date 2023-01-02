467660cc64499f150222d1884bce9327218296f1
KJS <nus0503@naver.com>
2023-01-03 02:37:50 +0900

    2023/01/03

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
-function Movie( {coverImg, title, summary, genres} ) {
+function Movie( {id, coverImg, title, summary, genres} ) {
     return <div>
     <img src={coverImg} alt={title} />
-    <h2>{title}</h2>
-    <p>{summary}</p>
+    <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+    </h2>
+    <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
     <ul>
         {genres.map(g => <li key={g}>{g}</li>)}
     </ul>
@@ -12,6 +15,7 @@ function Movie( {coverImg, title, summary, genres} ) {
 };
 
 Movie.propTypes = {
+    id : PropTypes.number.isRequired,
     coverImg : PropTypes.string.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,4 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 function Detail() {
-    return <h1>Detail</h1>
+    const [loading, setLoading] = useState(true);
+    const [movieDetail, setMovieDetail] = useState({});
+    const {id} = useParams();
+    const getMovie = useCallback(async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovieDetail(json.data.movie);
+        setLoading(false);
+        console.log(json);
+    }, [id]);
+    useEffect(() => {
+        getMovie();
+    }, [getMovie]);
+    console.log(movieDetail);
+    return (
+    <div>
+        {loading ? ( 
+            <h1>loading</h1> ) : (
+                <div key={movieDetail.id}>
+                    <img src={movieDetail.background_image} alt={movieDetail.id} />
+                    <ul>
+                        <li>{movieDetail.date_uploaded}</li>
+                        <li>{movieDetail.title}</li>
+                    </ul>
+                </div>
+            )
+        }
+    </div>
+    );
 };
 export default Detail;
